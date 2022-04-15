ea639160c753cce41118ec04cb3488c9cfa4bf7a
EL MERNISSI TARIK <83584683+elmernissitarik@users.noreply.github.com>
2022-04-15 06:32:18 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky_navbar");
+        } else {
+            $('.navbar').removeClass("sticky_navbar");
+        }
+
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function () {
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    $('.menu-btn').click(function () {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    var typed = new Typed(".typing", {
+        strings: ["Mechanical Engineer", "Web Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Mechanical Engineer", "Web Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+});
